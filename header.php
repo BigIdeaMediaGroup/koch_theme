f138436d4f7eb4aa667b991ae85755e75aa568f6
Will Clardy <will@quexxon.net>
2015-03-12 11:24:10 -0400

    Initialization; some css

--- header.php
+++ header.php
@@ -22,15 +22,29 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'bimg-base' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
+        <div itemscope itemtype="http://schema.org/Organization" class="site-contact">
+            <span itemprop="name" class="display-none">Christmas Decor</span>
+            Call Us Today! <span itemprop="telephone"></span>
+            <span itemprop="email"><?php bloginfo( 'admin_email' ) ?></span>
+        </div>
 		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<a class="site-title" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img class="logo"></a>
 		</div><!-- .site-branding -->
+        <div class="social-icons">
+        </div><!-- .social-icons -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'bimg-base' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
+    <header class="site-header sticky-header">
+        <div class"site-branding">
+        </div><!-- .site-branding -->
+		<nav id="site-navigation" class="main-navigation" role="navigation">
+			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'bimg-base' ); ?></button>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+		</nav><!-- #site-navigation -->
+    </header>
 
 	<div id="content" class="site-content">
